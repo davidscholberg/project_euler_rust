1febba3c1549eab1ed06251f328a375f1ecab27e
David Scholberg <recombinant.vector@gmail.com>
2023-06-05 18:10:00 -0400

    added arg parsing via clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Parser, Subcommand};
+
+/// Program for running Project Euler solutions.
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Runs the solution to a given Project Euler problem.
+    Solve {
+        /// The problem number to run the solution for.
+        problem_number: u64,
+    },
+}
+
 fn main() {
+    let _cli = Cli::parse();
     println!("Hello, world!");
 }
